169b0c2d9573cba0b7dc016a047373c3c375c68f
nzyokam <muusinzyoka@Euphoria.local>
2025-04-18 18:16:30 +0300

    updated main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pet import Pet
+
+#this is how you can use the class
+# Create a new pet instance
+my_pet = Pet("Simba")
+# Then Interact with the pet
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("roll over")
+my_pet.train("play dead")
+my_pet.show_tricks()
+my_pet.get_status()
--- pet.py
+++ pet.py
@@ -54,15 +54,3 @@ class Pet:
         print(f"  Happiness: {self.happiness}/10")
         print()
 
-#this is how you can use the class
-# Create a new pet instance
-my_pet = Pet("Simba")
-# Then Interact with the pet
-my_pet.get_status()
-my_pet.eat()
-my_pet.sleep()
-my_pet.play()
-my_pet.train("roll over")
-my_pet.train("play dead")
-my_pet.show_tricks()
-my_pet.get_status()
